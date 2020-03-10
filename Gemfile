84e016d77fac4adb4f97b56498c16b9271a7356e
Quang Long <longdq08@gmail.com>
2020-03-10 16:16:56 +0700

    Display pictures failed

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source :rubygems
+gem 'sinatra', '1.3.1'
+gem 'activerecord', '3.2.13'
+gem 'fileutils', '~> 1.4', '>= 1.4.1'
---
+++ up.rb
@@ -0,0 +1,45 @@
+<% Dir.foreach('public/uploads') do |picture| %>
+    <div class="gallery">
+      <a target="_blank" href="../../public/uploads/pig3.jpg">
+        <img src="../../public/uploads/pig3.jpg" alt="" width="600" height="400">
+      </a>
+      <div class="desc"><%= picture %></div>
+    </div>
+    
+    <% end %>
+
+    <!DOCTYPE html>
+<html>
+<head>
+<style>
+div.gallery {
+  margin: 5px;
+  border: 1px solid #ccc;
+  float: left;
+  width: 180px;
+}
+
+div.gallery:hover {
+  border: 1px solid #777;
+}
+
+div.gallery img {
+  width: 100%;
+  height: auto;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+</style>
+</head>
+<body>
+
+
+<div class="gallery">
+  <img src="../pic2.jpg" alt="" width="600" height="400">
+</div>
+
+</body>
+</html>
